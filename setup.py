e842cdb7c79f006aa16c355c6c23371d91c550cc
zakdoek <t_o_mvandamme@hotmail.com>
2016-05-11 08:32:30 +0200

    Prep for pypi

--- setup.py
+++ setup.py
@@ -14,19 +14,32 @@ import versioneer
 ###############################################################################
 
 NAME = "django-prosemirror"
+AUTHOR = "Tom Van Damme"
+EMAIL = "t_o_mvandamme@hotmail.com"
+URL = "https://github.com/zakdoek/django-prosemirror/"
+LICENCE = "MIT"
+DESCRIPTION = "Prosemirror field for django"
 PACKAGES = find_packages()
 CLASSIFIERS = [
-    "Private :: Do not upload",
+    "Framework :: Django",
 ]
 INSTALL_REQUIRES = []
 
 ###############################################################################
+
+README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
 
 
 if __name__ == "__main__":
     print(PACKAGES)
     setup(
         name=NAME,
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        licence=LICENCE,
+        description=DESCRIPTION,
+        long_description=README,
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
         packages=PACKAGES,
